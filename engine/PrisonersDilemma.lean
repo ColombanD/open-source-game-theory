b1f91dbfb9dbd15483d4be007dbec3bb240f8c25
Colomban <colomban.duclaux@hotmail.com>
2026-06-19 11:28:25 +0200

    bug: fix import errors

--- engine/PrisonersDilemma.lean
+++ engine/PrisonersDilemma.lean
@@ -1,8 +1,8 @@
 -- This module serves as the root of the `PrisonersDilemma` library.
 -- Import modules here that should be built as part of the library.
 import PrisonersDilemma.SizeLemmas
-import PrisonersDilemma.Computable
-import PrisonersDilemma.Demo
+import PrisonersDilemma.ComputableEval.Computable
+import PrisonersDilemma.ComputableEval.Demo
 import PrisonersDilemma.Theorems.CupodBot
 import PrisonersDilemma.Theorems.CooperateBot
 import PrisonersDilemma.Theorems.DefectBot
